bae125871967568470264ebb2ab64c1d046dcd84
kalekalek <87471596+kalekalek@users.noreply.github.com>
2021-07-23 12:56:53 -0400

    updated java

--- cut my grass please!!!!/my grass.js
+++ cut my grass please!!!!/my grass.js
@@ -12,5 +12,38 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
+if (confirm("thank you!")) {
+  txt = "you got services!";  
+} else {
+    txt = "You didnt finish!"
     
+}
+  const form = [ 
+ 'Name', 
+ 'Email', 
+  'Landscapping', 
+  'Debris Pickup', 
+  'General Labor',
+   'Sunday', 
+  'Monday', 
+  'Tuesday',
+'Wedsnesday', 
+'Thursday', 
+'Friday', 
+'Saturday'
+]; 
+
+console.log(form [0] );
+console.log(form [1] );
+console.log(form [2] );
+console.log(form [3] );
+console.log(form [4] );
+console.log(form [5] );
+console.log(form [6] );
+console.log(form [7] );
+console.log(form [8] );
+console.log(form [9] );
+console.log(form [10] );
+console.log(form [11] );
+
+alert
